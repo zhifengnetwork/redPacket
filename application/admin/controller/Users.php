ec8d631902aeeb3f93ff3fa1258926f8c802b91b
Rock-dav <1576648548@163.com>
2019-04-30 19:42:47 +0800

    后台：会员管理->会员列表 --未完成

---
+++ application/admin/controller/Users.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * 会员管理
+ */
+namespace app\admin\controller;
+
+use think\Db;
+
+class Users extends Common
+{
+    # 会员列表
+    public function index(){
+
+        $list = Db::name('users')->order('addtime desc')->paginate(15);
+
+
+        $this->assign('is_lock', [0=>'正常',1=>'已拉黑']);
+        $this->assign('list', $list);
+        return $this->fetch();
+    }
+
+    # 修改会员状态
+    public function edit_status(){
+        $id = isset($_POST['id']) ? intval($_POST['id']) : 0;
+        $is_lock = isset($_POST['is_lock']) ? intval($_POST['is_lock']) : 0;
+        if($id){
+            
+            $res = Db::name('users')->where('id',$id)->update(['is_lock' => $is_lock]);
+            if($res){
+                return json(['status'=>1]);
+            }
+        }
+        return json(['status'=>0]);
+    }
+
+
+}
--- application/extra/menu_config.php
+++ application/extra/menu_config.php
@@ -1,138 +1,34 @@
 <?php
 return [
-      //数据分析
-      'baobiao'      => [
-        'id'    => 60000,
-        'title' => '财务管理',
-        'sort'  => 2,
-        'url'   => 'total/index',
+    # 会员管理
+    'users' => [
+        'id'    => 10000,
+        'title' => '会员管理',
+        'sort'  => 21,
+        'url'   => 'users/index',
         'hide'  => 1,
-        'icon'  => 'glyphicon glyphicon-file',
+        'icon'  => 'glyphicon glyphicon-user',
         'child' => [
             [
-                'id'    => 60100,
-                'title' => '财务管理',
+                'id'    => 11000,
+                'title' => '会员管理',
                 'sort'  => 1,
-                'url'   => '',
-                'hide'  => 1,
-                'child' => [
-                    [
-                        'id'    => 60101,
-                        'title' => '余额记录',
-                        'sort'  => 1,
-                        'url'   => 'total/balance_logs',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 60102,
-                        'title' => '积分记录',
-                        'sort'  => 1,
-                        'url'   => 'total/integral_logs',
-                        'hide'  => 1,
-                    ],
-                ],
-
-            ],
-            [
-                'id'    => 60200,
-                'title' => '余额体现',
-                'sort'  => 2,
-                'url'   => 'total/finance',
-                'hide'  => 1,
-                'child' => [
-                    [
-                        'id'    => 60201,
-                        'title' => '待审核',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 60202,
-                        'title' => '通过审批',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 60203,
-                        'title' => '不通过审批',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 60204,
-                        'title' => '余额提现设置',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                   
-                ],
-            ],
-           
-         ],
-     ],
-
-
-
-    
-    
-    //配置管理
-    'pz_config' => [
-        'id'    => 50000,
-        'title' => '配置管理',
-        'sort'  => 8,
-        'url'   => 'config/index',
-        'hide'  => 1,
-        'icon'  => 'glyphicon glyphicon-link',
-        'child' => [
-            [
-                'id'    => 50100,
-                'title' => '首页轮播图',
-                'sort'  => 1,
-                'url'   => 'advertisement/index',
+                'url'   => 'users/index',
                 'hide'  => 1,
                 'icon'  => 'fa-th-large',
                 'child' => [
                     [
-                        'id'    => 50101,
-                        'title' => '首页轮播图',
+                        'id'    => 11100,
+                        'title' => '会员列表',
                         'sort'  => 1,
-                        'url'   => 'advertisement/index',
+                        'url'   => 'users/index',
                         'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 50201,
-                        'title' => '轮播图编辑',
-                        'sort'  => 1,
-                        'url'   => 'advertisement/edit',
-                        'hide'  => 0,
+                        'icon'  => 'fa-th-large',
                     ],
                 ],
             ],
-            [
-                'id'    => 50200,
-                'title' => '配送方式',
-                'sort'  => 2,
-                'url'   => 'delivery/index',
-                'icon'  => 'fa-th-large',
-                'hide'  => 1,
-            ],
-            [
-                'id'    => 50300,
-                'title' => '客服设置',
-                'sort'  => 3,
-                'url'   => 'config/get_config',
-                'icon'  => 'fa-th-large',
-                'hide'  => 1,
-            ],
-           
         ],
     ],
-
-
 
     //系统设置
     'sys_config'      => [
@@ -251,159 +147,6 @@ return [
                         'title' => '玩法规则',
                         'sort'  => 1,
                         'url'   => 'rule/setting',
-                        'hide'  => 1,
-                    ],
-                ],
-            ],
-        ],
-     ],
-
-    'statistics' => [
-        'id'    => 80000,
-        'title' => '统计中心',
-        'sort'  => 9,
-        'url'   => 'statistics/index',
-        'hide'  => 1,
-        'icon'  => 'glyphicon glyphicon-wrench',
-        'child' => [
-            [
-                'id'    => 80100,
-                'title' => '会员分析',
-                'sort'  => 1,
-                'url'   => '',
-                'hide'  => 1,
-                'icon'  => 'fa-th-large',
-                'child' => [
-                    [
-                        'id'    => 80101,
-                        'title' => '会员消费排行',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80102,
-                        'title' => '会员增长趋势',
-                        'sort'  => 2,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80103,
-                        'title' => '分销商增长趋势统计',
-                        'sort'  => 3,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80104,
-                        'title' => '会员积分统计',
-                        'sort'  => 4,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80105,
-                        'title' => '会员余额统计',
-                        'sort'  => 5,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80106,
-                        'title' => '会员现金消费统计',
-                        'sort'  => 6,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80107,
-                        'title' => '用户分析',
-                        'sort'  => 6,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                ],
-            ],
-            [
-                'id'    => 80200,
-                'title' => '销售分析',
-                'sort'  => 1,
-                'url'   => '',
-                'hide'  => 1,
-                'icon'  => 'fa-th-large',
-                'child' => [
-                    [
-                        'id'    => 80201,
-                        'title' => '销售统计',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80203,
-                        'title' => '销售指标',
-                        'sort'  => 2,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80204,
-                        'title' => '订单统计',
-                        'sort'  => 3,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80205,
-                        'title' => '流量入口统计',
-                        'sort'  => 4,
-                        'url'   => 'distribution/distribution_grade',
-                        'hide'  => 1,
-                    ],
-                ],
-            ],
-            [
-                'id'    => 80300,
-                'title' => '商品分析',
-                'sort'  => 1,
-                'url'   => '',
-                'hide'  => 1,
-                'icon'  => 'fa-th-large',
-                'child' => [
-                    [
-                        'id'    => 80301,
-                        'title' => '商品销售明细',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80302,
-                        'title' => '商品销售排行',
-                        'sort'  => 2,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80303,
-                        'title' => '商品销售转化率',
-                        'sort'  => 3,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80304,
-                        'title' => '入驻商家销售排行',
-                        'sort'  => 4,
-                        'url'   => 'distribution/distribution_grade',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 80305,
-                        'title' => '入驻商家流量排行',
-                        'sort'  => 4,
-                        'url'   => 'distribution/distribution_grade',
                         'hide'  => 1,
                     ],
                 ],
