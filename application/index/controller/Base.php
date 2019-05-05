2fbba2646e91e01c4bb9ee272f225eede56f9209
zr <zr@qq.com>
2019-05-05 21:07:29 +0800

    添加前端聊天测试页面以及控制器代码

---
+++ application/index/controller/Base.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\index\controller;
+// use app\common\model\Config;
+use think\Controller;
+use think\Db;
+use think\Request;
+use think\Session;
+use think\View;
+
+/*
+ * 公共控制器
+ */
+class Base extends Controller
+{
+    /*
+     * 初始化
+     */
+    public function _initialize()
+    {
+        
+    }
+
+}
