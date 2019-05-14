c2380d532c38f16a97582086677142ca87a05fd9
cooper <xieboyboy@163.com>
2019-05-14 14:06:04 +0800

    修改index.php控制器

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,6 +4,16 @@ namespace app\index\controller;
 class Index extends Base
 {
 
+
+	public function _initialize()
+    {   
+        if (is_user_login()) {
+            //登陆跳转到消息页
+            $this->redirect('index/message/messageList');
+            exit;
+        }
+    }
+
     // 登录页面
     public function index()
     {    
