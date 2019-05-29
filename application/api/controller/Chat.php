0ea49339aa390bf0d5f7bf596e73f6420f170872
zr <zr@qq.com>
2019-05-29 10:06:13 +0800

    添加红包超时退回功能

--- application/api/controller/Chat.php
+++ application/api/controller/Chat.php
@@ -10,6 +10,8 @@ namespace app\api\controller;
 use think\Controller;
 use think\Db;
 use think\Request;
+use think\Config;
+
 //  聊天相关API数据处理
 class Chat extends Controller{
 
@@ -294,20 +296,66 @@ class Chat extends Controller{
      */
     public function sendBackRed()
     {
-        // if(!Request::instance()->isAjax()){
-        //     return json(['code'=>0, 'msg'=>'非法请求', 'data'=>'']);
-        // }
+        if(!Request::instance()->isAjax()){
+            return json(['code'=>0, 'msg'=>'非法请求', 'data'=>'']);
+        }
         // 获取所有大于5分钟的红包
-        // $this->send_key = Config::get('SEND_KEY');
+        $send_key = Config::get('SEND_KEY');
         // $key = input('key/s');
+        // if(!$key || $send_key!=$key){
+        //     echo 'no...';exit;
+        // }
         $map['m.time_out'] = 0;
         $map['m.all_get'] = 0;
         $red_all = Db::name('chat_red_master')->alias('m')
-                ->join('chat_red_detail d','m.id=d.m_id')
+                ->field('m.id,m.uid,m.room_id,m.num,m.money,m.create_time,m.time_out,m.all_get')
                 ->where('m.time_out',0)
-                ->fetchSql(true)
                 ->select();
-        dump($red_all);        
+        // 如果超时退回标记主表time_out=1 以及从表type=2，并且统计红包是否全部领取，如果全部领取标记主表all_get=1
+        $out_time = 350; // 过期时间
+        $i = 0;
+        if($red_all){
+            foreach($red_all as $k=>$v){
+                // 判断当前时间是否大于等于红包创建时间+过期时间
+                if(time() >= $v['create_time']+$out_time){
+                    // 根据当前主表id获取从表没被抢的红包记录 统计没被领取红包总金额
+                    $no_get_money = Db::name('chat_red_detail')->where(['m_id'=>$v['id'], 'type'=>0])->sum('money');
+                    // 退还金额到对应用户
+                    if($no_get_money){
+                        $out_money_res = Db::name('users')->where(['id'=>$v['uid']])->setInc('account', $no_get_money);
+                        if($out_money_res){
+                            // 修改状态
+                            $out_update_res = Db::name('chat_red_detail')->where(['m_id'=>$v['id'], 'type'=>0])->update(['type'=>2]);
+                            $out_update_res2 = Db::name('chat_red_master')->where(['id'=>$v['id']])->update(['time_out'=>1,'all_get'=>1]);
+                            // 插入日志
+                            $out_money_log = [
+                                'from_id' => $v['uid'],
+                                'uid' => $v['uid'],
+                                'm_id' => $v['id'],
+                                'red_money' => $v['money'],
+                                'money' => $no_get_money,
+                                'type' => 12,
+                                'create_time' => time(),
+                                'remake' => '红包退回'
+                            ];
+                            $out_money_log_res = Db::name('chat_red_log')->insert($out_money_log);
+                        }else{
+                            echo 'out red update red err\n';
+                        }
+                    }else{
+                        // 修改主表标记全部领取
+                        $out_update_res3 = Db::name('chat_red_master')->where(['id'=>$v['id']])->update(['all_get'=>1]);
+                        continue;
+                    }
+                }
+                $i++;
+            }
+            echo 'out red '.$i;
+        }else{
+            echo 'no order\n';
+            exit;
+        }
+
 
     }
 }
