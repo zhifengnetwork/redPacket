0da1139ecb9ab78094b8698d0e275a4dbb31ad25
jialonghub <3077241433@qq.com>
2019-06-01 11:46:14 +0800

    海报页面优化

--- application/index/controller/Find.php
+++ application/index/controller/Find.php
@@ -1,12 +1,19 @@
 <?php
 namespace app\index\controller;
-
+use think\Db;
 class Find extends Base
 {
 
     // 发现页面
     public function index()
     {    
+
+    	
+    	$info = Db::table('poster')->where('title','海报')->field('url')->find();
+
+    	$this->assign('img', $info);
+
+
         return $this->fetch('find');
     }
 
