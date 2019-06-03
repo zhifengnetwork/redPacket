0f93eb2bbd2de360315fe8881540c871528398bc
hlb243X <2726724651@qq.com>
2019-06-03 13:57:42 +0800

    我的-项-页面跳转

--- public/static/chatWeb/js/public/public.js
+++ public/static/chatWeb/js/public/public.js
@@ -12,7 +12,7 @@ function returnFun(){
 		console.log("返回上一页");
 	}else {
 		/*页面跳转*/
-		window.location.href = $('.lb_headWrap .lb_headWrap_return').attr('data-num');
+		window.location.href =  return_url;
 		console.log("页面跳转");
 	}
 	return false;
