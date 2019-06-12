d48747b6b50b036cfde324b45c8bb5e2fc02a6c6
服务器 <yudwcom@163.com>
2019-06-13 02:48:59 +0800

    修改报错

--- application/index/controller/Address.php
+++ application/index/controller/Address.php
@@ -122,12 +122,20 @@ class Address extends Base
         }
         $data=[];
         foreach ($list as $ks => $vs) {
-            if ( empty( $data[ $vs['chart'] ] ) ) {
-                $data[ $vs['chart'] ]= [];
+            if(empty($vs['head_imgurl'])){
+                $vs['head_imgurl'] = "/static/chatWeb/img/00avatar-01-zp.png";
+                $vs['nickname'] = "默认用户";
             }
-            $data[ $vs['chart'] ][] = $vs;
+            
+            // if ( empty( $data[ $vs['chart'] ] ) ) {
+            //     $data[ $vs['chart'] ]= [];
+            // }else{
+                $data[$vs['chart']][] = $vs;
+            // }
         }
+        
         ksort($data);
+        // dump($data);die;
         return $data;
     }
 
@@ -137,6 +145,7 @@ class Address extends Base
     * @return [type]      [strind]
     */
     protected function getFirstChart($str){
+        // dump($str);
         if( empty($str) ){
             return '';
         }
@@ -144,35 +153,123 @@ class Address extends Base
         if( $char >= ord('A') && $char <= ord('z') ){
             return strtoupper($str[0]);
         } 
-        // $s1 = iconv('UTF-8','gb2312',$str);
-        // $s2 = iconv('gb2312','UTF-8',$s1);
-        // $s = $s2 == $str?$s1:$str;
-        $s = $str;
+        $s0 = mb_substr($str,0,3); //获取名字的姓
+        // dump($s0);
+        $s1 = iconv('UTF-8','gb2312//IGNORE',$str);
+        $s2 = iconv('gb2312','UTF-8',$s1);
+        $s = $s2 == $str?$s1:$str;
+        
+        // if($str=='嗯'){
+        //     echo $s;die;
+        // }
+        // $s = iconv("utf-8","gb2312//IGNORE",$str);
+        // // $s = iconv('UTF-8','gb2312', $s0); //将UTF-8转换成GB2312编码
+        // $s = $str;
+        // dump($s);
+        if (ord($s0)>128) { //汉字开头，汉字没有以U、V开头的
         $asc=ord($s{0})*256+ord($s{1})-65536;
-            if($asc>=-20319&&$asc<=-20284) return 'A';
-            if($asc>=-20283&&$asc<=-19776) return 'B';
-            if($asc>=-19775&&$asc<=-19219) return 'C';
-            if($asc>=-19218&&$asc<=-18711) return 'D';
-            if($asc>=-18710&&$asc<=-18527) return 'E';
-            if($asc>=-18526&&$asc<=-18240) return 'F';
-            if($asc>=-18239&&$asc<=-17923) return 'G';
-            if($asc>=-17922&&$asc<=-17418) return 'H';
-            if($asc>=-17417&&$asc<=-16475) return 'J';
-            if($asc>=-16474&&$asc<=-16213) return 'K';
-            if($asc>=-16212&&$asc<=-15641) return 'L';
-            if($asc>=-15640&&$asc<=-15166) return 'M';
-            if($asc>=-15165&&$asc<=-14923) return 'N';
-            if($asc>=-14922&&$asc<=-14915) return 'O';
-            if($asc>=-14914&&$asc<=-14631) return 'P';
-            if($asc>=-14630&&$asc<=-14150) return 'Q';
-            if($asc>=-14149&&$asc<=-14091) return 'R';
-            if($asc>=-14090&&$asc<=-13319) return 'S';
-            if($asc>=-13318&&$asc<=-12839) return 'T';
-            if($asc>=-12838&&$asc<=-12557) return 'W';
-            if($asc>=-12556&&$asc<=-11848) return 'X';
-            if($asc>=-11847&&$asc<=-11056) return 'Y';
-            if($asc>=-11055&&$asc<=-10247) return 'Z';
-            return null;
+            if($asc>=-20319 and $asc<=-20284)return "A";
+            if($asc>=-20283 and $asc<=-19776)return "B";
+            if($asc>=-19775 and $asc<=-19219)return "C";
+            if($asc>=-19218 and $asc<=-18711)return "D";
+            if($asc>=-18710 and $asc<=-18527)return "E";
+            if($asc>=-18526 and $asc<=-18240)return "F";
+            if($asc>=-18239 and $asc<=-17760)return "G";
+            if($asc>=-17759 and $asc<=-17248)return "H";
+            if($asc>=-17247 and $asc<=-17418)return "I";
+            if($asc>=-17417 and $asc<=-16475)return "J";
+            if($asc>=-16474 and $asc<=-16213)return "K";
+            if($asc>=-16212 and $asc<=-15641)return "L";
+            if($asc>=-15640 and $asc<=-15166)return "M";
+            if($asc>=-15165 and $asc<=-14923)return "N";
+            if($asc>=-14922 and $asc<=-14915)return "O";
+            if($asc>=-14914 and $asc<=-14631)return "P";
+            if($asc>=-14630 and $asc<=-14150)return "Q";
+            if($asc>=-14149 and $asc<=-14091)return "R";
+            if($asc>=-14090 and $asc<=-13319)return "S";
+            if($asc>=-13318 and $asc<=-12839)return "T";
+            if($asc>=-12838 and $asc<=-12557)return "W";
+            if($asc>=-12556 and $asc<=-11848)return "X";
+            if($asc>=-11847 and $asc<=-11056)return "Y";
+            if($asc>=-11055 and $asc<=-10247)return "Z";
+        }else if(ord($s)>=48 and ord($s)<=57)
+        { //数字开头
+            switch(iconv_substr($s,0,1,'utf-8')){
+                case 1:return "Y";
+                case 2:return "E";
+                case 3:return "S";
+                case 4:return "S";
+                case 5:return "W";
+                case 6:return "L";
+                case 7:return "Q";
+                case 8:return "B";
+                case 9:return "J";
+                case 0:return "L";
+            }
+        }else if(ord($s)>=65 and ord($s)<=90){//大写英文开头
+            return substr($s,0,1);
+        }else if(ord($s)>=97 and ord($s)<=122){//小写英文开头
+            return strtoupper(substr($s,0,1));
+        }else{
+            return iconv_substr($s0,0,1,'utf-8');
+            //中英混合的词语，不适合上面的各种情况，因此直接提取首个字符即可
+        }
+    }
+
+    public function getFirstChart1($str){
+        $s0 = mb_substr($str,0,3); //获取名字的姓
+        $s = $str;
+        // $s = iconv('UTF-8','gb2312', $s0); //将UTF-8转换成GB2312编码
+        // dump($s0);
+        if (ord($s0)>128) { //汉字开头，汉字没有以U、V开头的
+        $asc=ord($s{0})*256+ord($s{1})-65536;
+        if($asc>=-20319 and $asc<=-20284)return "A";
+        if($asc>=-20283 and $asc<=-19776)return "B";
+        if($asc>=-19775 and $asc<=-19219)return "C";
+        if($asc>=-19218 and $asc<=-18711)return "D";
+        if($asc>=-18710 and $asc<=-18527)return "E";
+        if($asc>=-18526 and $asc<=-18240)return "F";
+        if($asc>=-18239 and $asc<=-17760)return "G";
+        if($asc>=-17759 and $asc<=-17248)return "H";
+        if($asc>=-17247 and $asc<=-17418)return "I";
+        if($asc>=-17417 and $asc<=-16475)return "J";
+        if($asc>=-16474 and $asc<=-16213)return "K";
+        if($asc>=-16212 and $asc<=-15641)return "L";
+        if($asc>=-15640 and $asc<=-15166)return "M";
+        if($asc>=-15165 and $asc<=-14923)return "N";
+        if($asc>=-14922 and $asc<=-14915)return "O";
+        if($asc>=-14914 and $asc<=-14631)return "P";
+        if($asc>=-14630 and $asc<=-14150)return "Q";
+        if($asc>=-14149 and $asc<=-14091)return "R";
+        if($asc>=-14090 and $asc<=-13319)return "S";
+        if($asc>=-13318 and $asc<=-12839)return "T";
+        if($asc>=-12838 and $asc<=-12557)return "W";
+        if($asc>=-12556 and $asc<=-11848)return "X";
+        if($asc>=-11847 and $asc<=-11056)return "Y";
+        if($asc>=-11055 and $asc<=-10247)return "Z";
+        }else if(ord($s)>=48 and ord($s)<=57){ //数字开头
+            switch(iconv_substr($s,0,1,'utf-8')){
+                case 1:return "Y";
+                case 2:return "E";
+                case 3:return "S";
+                case 4:return "S";
+                case 5:return "W";
+                case 6:return "L";
+                case 7:return "Q";
+                case 8:return "B";
+                case 9:return "J";
+                case 0:return "L";
+            }
+        }else if(ord($s)>=65 and ord($s)<=90){ //大写英文开头
+            return substr($s,0,1);
+        }else if(ord($s)>=97 and ord($s)<=122){ //小写英文开头
+            return strtoupper(substr($s,0,1));
+        }
+        else
+        {
+            return iconv_substr($s0,0,1,'utf-8');
+            //中英混合的词语，不适合上面的各种情况，因此直接提取首个字符即可
+        }
     }
 
 
